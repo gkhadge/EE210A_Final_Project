6269327219b7ed0d49ece89b65e23db768349920
jzheng <jwzheng94@gmail.com>
2017-02-27 22:01:14 -0800

    implemented m step of baum welch (untested)

--- Word.m
+++ Word.m
@@ -99,14 +99,14 @@ classdef Word < handle %inherit from handle so all copies reference this one cla
         % mu, and Sigma
 		% implement assuming 1 set of observations first, extend to multiple sets later
         % r NxL (num states x length of observation)
-        % S L-1xNxN (L-1 NxN matrices)
+        % S NxNxL-1 (L-1 NxN matrices)
         % Nr Nx1
         function [r,S,Nr] = e_step(self, observations)
             D = self.state_likelihood(observations);
             [alpha, beta, c] = self.forward_backward(observations, D);
             L = size(observations,2);
             
-            r = alpha.*beta;
+            r = alpha.*beta;            %size NxL (L length of observation vector, N number of states)
             S = zeros(self.N, self.N, L-1);
             for i = 2:L
                 S(:,:,i) = ((self.A).*(alpha(:,i-1)*(beta(:,i).*D(:,i))'))/c(i);
@@ -118,8 +118,43 @@ classdef Word < handle %inherit from handle so all copies reference this one cla
 		% observation set will probably have to be a cell matrix since each set of 
 		% observations can be a different length
 		function trainAll(self, observation_set, num_iter)
-		end
+            L = size(observation_set,2);
+            expected_mu = zeros(size(self.mu));     %size NxD (D size of feature vector)
+            expected_Sigma = zeros(size(self.Sigma));
+            expected_prior_num = zeros(size(self.prior));
+            expected_prior_den = 0;
+            expected_A_num = zeros(size(self.A));
+            expected_A_den = zeros(size(self.A));
+            expected_N = zeros(self.N,1);
+            for i = 1:num_iter
+                for l = 1:L
+                    Y = cell2mat(observation_set{l});
+                    Nl = size(Y,2);
+                  	[r,S,Nr] = self.e_step(Y);
+                    expected_mu = expected_mu + r*Y';
+                    for j = 1:self.N
+                        mu_k = repmat(self.mu(:,j),1,Nl); %[mu_k mu_k ... mu_k]
+                        r_k = repmat(r(j,:)',Nl,1); 
+                        expected_Sigma(:,:,j) = expected_Sigma(:,:,j) + (r_k.*(Y-mu_k))*(Y-mu_k)';
+                    end
+                    expected_prior_num = expected_prior_num + r(:,1);
+                    expected_prior_den = expected_prior_den + sum(r(:,1));
+                    expected_A_num = expected_A_num + sum(S,3);
+                    expected_A_den = expected_A_den + repmat(sum(sum(S,3)),self.N,1);
+                    expected_N = expected_N + Nr;    
+                end
+                
+                for j = 1:self.N
+                    self.mu(:,j) = expected_mu(:,j)/expected_N(j);
+                    self.Sigma(:,:,j) = expected_Sigma(:,:,j)/expected_N(j);
+                end
+                self.prior = expected_prior_num/expected_prior_den;
+                self.A = expected_A_num./expected_A_den;
+            end
+        end
         
+        function initialize(self, observations)
+        end
         %add functions as needed
       
         
