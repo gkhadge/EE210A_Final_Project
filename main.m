58188ed338c603bda68fab3fd7953870968afb9d
Jason Zheng <jwzheng94@gmail.com>
2017-03-06 00:01:56 -0800

    added some test code to plot apple feature vectors

--- main.m
+++ main.m
@@ -1,2 +1,16 @@
 %Jason Zheng & Gourav Khadge
 %EE210A Speech Recognition
+
+[audio_signals, word_labels] = load_audio('audio');
+apple = Word('apple');
+
+apple_signals = {};
+
+%plot all the feature vectors for apple signals
+for i = 1:15
+    apple_signals(i) = {extract_features(audio_signals{i})};
+    scatter(apple_signals{i}(1,:), apple_signals{i}(2,:));
+    hold on;
+end
+    
+axis([0 4000 0 4000]);
